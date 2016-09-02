0a19c962e00685091a8c6acf57514077efd7a512
stepan001 <stepan001@mail.ru>
2016-09-02 18:09:25 +0300

    jQuery refactoring, added preventDefault

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-// $(".embframe").hide();
-$(".emba").click(function(){
-	// $(this).hide();
-	// $(this).next().show();
-		
+$(".emba").click(function(event){
+	event.preventDefault();
+			
 	$(this).replaceWith(function() {
 		if ($(this).parent().hasClass("col1")) {
 			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=327680&width=&height=&lang=ru"></iframe>';	
@@ -13,7 +11,7 @@ $(".emba").click(function(){
 		else if ($(this).parent().hasClass("col3")) {
 			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=458752&width=&height=&lang=ru"></iframe>';	
 		}
-		else  {
+		else {
 			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=524288&width=&height=&lang=ru"></iframe>';	
 		}
 	})
