0dbd3df9727e45364ff7b9b440610a475bb90d61
stepan001 <stepan001@mail.ru>
2016-09-02 18:00:03 +0300

    jQuery refactoring, added replaceWith method

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-$(".embframe").hide();
+// $(".embframe").hide();
 $(".emba").click(function(){
-	$(this).hide();
-	$(this).next().show();
-	// $(".embframe").show();
+	// $(this).hide();
+	// $(this).next().show();
+		
+	$(this).replaceWith(function() {
+		if ($(this).parent().hasClass("col1")) {
+			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=327680&width=&height=&lang=ru"></iframe>';	
+		}
+		else if ($(this).parent().hasClass("col2")) {
+			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=393216&width=&height=&lang=ru"></iframe>';	
+		}
+		else if ($(this).parent().hasClass("col3")) {
+			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=458752&width=&height=&lang=ru"></iframe>';	
+		}
+		else  {
+			return '<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=524288&width=&height=&lang=ru"></iframe>';	
+		}
+	})
 })
