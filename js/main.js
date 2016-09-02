ce1f33267efe7c85565cc9ec6cd6474834a7fd61
stepan001 <stepan001@mail.ru>
2016-09-03 00:07:00 +0300

    Added dummy image logic

--- js/main.js
+++ js/main.js
@@ -3,13 +3,20 @@ var $ifr2 = $('<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2
 var $ifr3 = $('<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=458752&width=&height=&lang=ru&ap=&noibw="></iframe>');
 var $ifr4 = $('<iframe class="embframe" src="https:\/\/open.ivideon.com/embed/v2/?server=100-e9bbe8d43447648a9f679f0b724824e6&camera=524288&width=&height=&lang=ru&ap=&noibw="></iframe>');
 var $ifrReplaced;
+var dummyImage = '<img src="img/pereryv.png" alt="Страница временно недоступна">';
+
+var $temp = $("img").on("error", function(){
+		$(this).parents("a").replaceWith(function() {
+			return dummyImage;
+		});
+});
+
 $(".emba").click(function(event){
 	event.preventDefault();
 			
 	$(this).replaceWith(function() {
 		if ($(this).parent().hasClass("col1")) {
 			$ifrReplaced = $ifr1;
-			console.log($ifrReplaced);
 			return $ifrReplaced;	
 		}
 		else if ($(this).parent().hasClass("col2")) {
@@ -24,5 +31,5 @@ $(".emba").click(function(event){
 			$ifrReplaced = $ifr4;
 			return $ifrReplaced;	
 		}
-	})
-})
+	});
+});
